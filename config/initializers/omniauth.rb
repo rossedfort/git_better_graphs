2d43ea6ff70991c35f3ca9fddcf2280e1b3edf04
rossedfort <rossedfort@gmail.com>
2016-01-20 16:39:28 -0700

    precompile assets, add production keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :github, ENV['github_key'], ENV['github_secret']
+    provider :github, ENV['github_key_pro'], ENV['github_secret_pro']
 end
--- public/assets/application-bb634594747b3013d6d24146776a553803a715af8699eb0e3bc81f424bea2824.js
+++ public/assets/application-bb634594747b3013d6d24146776a553803a715af8699eb0e3bc81f424bea2824.js
@@ -27246,6 +27246,63 @@ function buildLanguageGraph() {
   }
 }
 ;
+function buildPullRequestGraph() {
+  $(".pullRequestLoader").show();
+  $.getJSON("/users/" + userName + "/repos/" + repoName + "/pull_requests").then(drawPullRequestGraph).always(function() {
+    $(".pullRequestLoader").hide();
+  });
+
+  function drawPullRequestGraph(data) {
+    var w = 700;
+    var h = 400;
+    var r = h/2;
+    var legendRectSize = 18;
+    var legendSpacing = 4;
+    var color = d3.scale.category20c();
+    var svg = d3.select('#pullRequestGraph')
+    .append("svg:svg").data([data])
+    .attr("width", w)
+    .attr("height", h)
+    .append("svg:g")
+    .attr("transform", "translate(" + r + "," + r + ")");
+    var pie = d3.layout.pie().value(function(d){return d.value;});
+    var arc = d3.svg.arc().outerRadius(r);
+    var arcs = svg.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
+
+    arcs.append("svg:path")
+    .attr("fill", function(d, i){
+      return color(i);
+    })
+    .attr("d", function (d) {
+      return arc(d);
+    });
+
+    var legend = svg.selectAll('.legend')
+    .data(color.domain())
+    .enter()
+    .append('g')
+    .attr('class', 'legend')
+    .attr('transform', function(d, i) {
+      var height = legendRectSize + legendSpacing;
+      var offset =  height * color.domain().length / 2;
+      var horz = 20 * legendRectSize;
+      var vert = i * height - offset;
+      return 'translate(' + horz + ',' + vert + ')';
+    });
+
+    legend.append('rect')
+    .attr('width', legendRectSize)
+    .attr('height', legendRectSize)
+    .style('fill', color)
+    .style('stroke', color);
+
+    legend.append('text')
+    .attr('x', legendRectSize + legendSpacing)
+    .attr('y', legendRectSize - legendSpacing)
+    .text(function(d, i) { return data[i].label; });
+  }
+}
+;
 function getRepoData() {
   $.ajax({
     type:    "GET",
@@ -27416,6 +27473,7 @@ $(document).ready(function(){
   buildContributorGraph();
   getCommitActivity();
   buildFrequencyGraph();
+  buildPullRequestGraph();
   $("#languageGraphButton").click(function() {
     $(".graphs").children().addClass("hidden")
     $("#languageDataGraph").toggleClass( "hidden" );
@@ -27431,5 +27489,9 @@ $(document).ready(function(){
   $("#frequencyGraphButton").click(function() {
     $(".graphs").children().addClass("hidden")
     $("#frequencyGraph").toggleClass( "hidden" );
+  });
+  $("#pullRequestGraphButton").click(function() {
+    $(".graphs").children().addClass("hidden")
+    $("#pullRequestGraph").toggleClass( "hidden" );
   });
 });
