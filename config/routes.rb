f7accf6b4d5212c620a43d8d350c61660235c2ed
rossedfort <rossedfort@gmail.com>
2016-02-28 13:14:44 -0700

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'root#welcome'
 
-  get 'auth/github', as: :login
-  get 'auth/github/callback', to: 'sessions#create'
+  get '/auth/github', as: :login
+  get '/auth/github/callback', to: 'sessions#create'
 
   get '/logout', to: 'sessions#destroy'
 
